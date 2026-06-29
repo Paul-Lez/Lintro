40dc563d72b5e29a7eb656fbe40abc02ca608fa9
Paul Lezeau <paul.lezeau@gmail.com>
2026-06-29 09:58:12 +0100

    a few edits

--- Lintro/Basic.lean
+++ Lintro/Basic.lean
@@ -43,7 +43,7 @@ This file contains a few basic demos to show how Lean works
 /- The basic syntax -/
 
 theorem my_first_theorem : 1 + 1 = 2 := by
-  rfl
+  sorry
 
 /-- Triangular numbers are numbers of the form ∑_{i ≤ 0}^n i. -/
 def TriangularNumber (n : ℕ) : ℕ := ∑ i ∈ Finset.Icc 1 n, i
@@ -51,10 +51,8 @@ def TriangularNumber (n : ℕ) : ℕ := ∑ i ∈ Finset.Icc 1 n, i
 theorem triangularNumber_eq (n : ℕ) : TriangularNumber n = Nat.choose n 2 := by
   rw [TriangularNumber]
   induction n with
-  | zero => simp
-  | succ n ih =>
-    sorry
-    -- rw [←Nat.succ_eq_add_one, ←Nat.succ_eq_succ]
+  | zero => sorry
+  | succ n ih => sorry
 
 
 
@@ -95,7 +93,10 @@ theorem triangularNumber_eq (n : ℕ) : TriangularNumber n = Nat.choose n 2 := b
 
 /- Tactics -/
 
-example (P : Prop) (h : P) : P := by
+example (P Q : Prop) (P_implies_Q : P → Q) (h : P) : Q := by
+  sorry
+
+example (a b c : Nat) (H : a ∣ b) : a ∣ b * c := by
   sorry
 
 example : ∀ x y, (x + y)^2 = x^2 + 2 * x * y + y^2 := by
@@ -107,8 +108,9 @@ example (a b c : ℚ) (hab : a ≤ b) (hbc : b ≤ c) : a + 1 ≤ c + 1 := by
 example : Nat.Prime 101 := by
   sorry
 
-example (a b c : Nat) (H : a ∣ b) : a ∣ b * c := by
-  sorry
+example : (10 ^ 81 - 9) * Real.pi
+  = 999999999999999999999999999999999999999999999999999999999999999999999999999999991 * Real.pi := by
+  norm_num
 
 
 
@@ -149,16 +151,12 @@ end
 Algebraic structures
 -/
 
--- Let `G` be a group
-variable {G : Type} [Group G]
-variable (a b c : G)
+-- Let `G` be a group:
 
--- We can use the usual multiplicative notation
-example : a * (b * c) = (a * b) * c :=
-  (mul_assoc a b c).symm
+-- We can use the usual multiplicative notation:
 
--- Let `R` be a commutative ring
-variable {R : Type} [CommRing R]
+
+-- Let `R` be a commutative ring:
 
 -- Many of the structures you'll need are defined in Mathlib already
 -- #synth Ring ℤ
@@ -224,10 +222,10 @@ example (n : Nat) (h : n ∈ ({m : Nat | m <= 5} : Set Nat)) : n <= 5 := by
 
 /- Definitions vs theorems -/
 
+-- The first isomorphism theorem
 -- variable {G G' : Type} [Group G] [Group G'] (ψ : G →* G')
 
--- theorem first_isomorphism_theorem : G ⧸ ψ.ker ≃* ψ.range :=
-  -- QuotientGroup.quotientKerEquivRange ψ
+-- QuotientGroup.quotientKerEquivRange ψ
 
 
 
